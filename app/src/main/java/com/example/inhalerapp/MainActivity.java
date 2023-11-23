8b41922081a216c37a7d6279571937a73b9a9497
Patrick <patrick.langer97@gmx.de>
2023-11-23 09:58:51 +0100

    Fixed permissions.

--- app/src/main/java/com/example/inhalerapp/MainActivity.java
+++ app/src/main/java/com/example/inhalerapp/MainActivity.java
@@ -29,6 +29,7 @@ import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CODE = 123;
+    private BluetoothManager bluetoothManager;
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothLeScanner bluetoothLeScanner;
     private TextView connectionStatus;
@@ -41,15 +42,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Permissions for BLE scanning in Android 12 and above
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, PERMISSION_REQUEST_CODE);
-        } else {
-            startBleScan();
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
         }
-
         connectionStatus = findViewById(R.id.connection_status);
 
+
+    }
+
+    private void startBleScan() {
+
         // Initialize Bluetooth manager and adapter
-        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         if (bluetoothManager != null) {
             bluetoothAdapter = bluetoothManager.getAdapter();
         }
@@ -57,15 +65,17 @@ public class MainActivity extends AppCompatActivity {
         // Check if Bluetooth is available and on
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
             // Handle the case where device does not support Bluetooth or it is not enabled
-            connectionStatus.setText("Bluetooth not available");
+            connectionStatus.setText("Bluetooth not available, please turn on bluetooth");
+            return;
         } else {
             bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+            connectionStatus.setText("Bluetooth available");
         }
-    }
 
-    private void startBleScan() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED) {
             Log.d("BLE", "Start scanning.");
+            connectionStatus.setText("Bluetooth available");
+
             bluetoothLeScanner.startScan(new ScanCallback() {
                 @Override
                 public void onScanResult(int callbackType, ScanResult result) {
@@ -89,7 +99,11 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("Permission", "Permission is granted.");
+                for(int i = 0; i < grantResults.length; i++)
+                {
+                    Log.d("Permission", "Permission is granted. " + grantResults[i]);
+
+                }
                 startBleScan();
             } else {
                 Log.d("Permission", "Permission is denied.");
